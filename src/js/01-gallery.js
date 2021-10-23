d2712210dcb9755e47b105051def1bf329d605e9
Serhii-Khrapun <Sergiy.Khrapun@gmail.com>
2021-10-23 21:40:25 +0300

    зробив 1 завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryElement = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createImg = galleryItems
+  .map(
+    (galleryItem) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>`
+  )
+  .join(" ");
+
+galleryElement.insertAdjacentHTML("afterbegin", createImg);
+
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  const url = event.target.dataset.source;
+
+  const modal = basicLightbox
+    .create(
+      `
+		<img  src="${url}">
+	`
+    )
+    .show();
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryElement = document.querySelector(".gallery");
+
+const createImg = galleryItems
+  .map(({preview, original,description}) =>
+    
+      `<a class="gallery__item" href="${original}">
+     <img class="gallery__image" src="${preview}" 
+    alt="${description}" />
+</a>`
+  )
+  .join(" ");
+
+galleryElement.insertAdjacentHTML("afterbegin", createImg);
+
+
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+function onGalleryContainerClick (event) {
+    event.preventDefault();
+ 
+    const url = event.target.dataset.source;
+    var lightbox = new SimpleLightbox('.gallery a');
+    var lightbox = $('.gallery a').simpleLightbox();
+ }
+ 
